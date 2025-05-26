aa493bc8906523489267ac10c6a3072640ce0145
Carlos Eduardo <104609611+DanonePlayer@users.noreply.github.com>
2025-05-26 17:30:53 -0500

    Initial commit

---
+++ desafio/src/main/java/com/desafio/desafio/infra/cliente/models/clienteModel.java
@@ -0,0 +1,11 @@
+package com.desafio.desafio.infra.cliente.models;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class clienteModel {
+    private Long id;
+    private String nome;
+}
